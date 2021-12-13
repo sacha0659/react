62dfa7db429d56c2bdbce819b08e5016e8c957b4
sacha <sachagaleuzzi@gmail.com>
2021-12-13 14:37:09 +0100

    [nav bar] ajout de navbar et de style

---
+++ app/components/Navbar.js
@@ -0,0 +1,16 @@
+
+import react,{ useEffect, useState } from 'react'
+import styles from '../styles/Navbar.module.css'
+
+export default function Navbar(){
+return(
+
+    <div className={styles.navcontainer}>
+        <p>toto</p>
+        <p>test2</p>
+        <p>test3</p>
+    </div>
+
+
+)
+}
